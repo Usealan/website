b2be607c0f6d8b9693b04615807284b2bf7444d3
Jorge <jorge@mexbowl.com>
2021-09-13 09:22:02 -0500

    learn more button

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,3 +2,12 @@ $(".coming-soon-alert").click(function(e) {
   e.preventDefault();
   alert($(this).attr("data-alert"));
 });
+
+$(".go-to-main-block").on("click", function() {
+  event.preventDefault();
+  $("body").removeClass("nav-active");
+  $("html,body").removeClass("frozen");
+  $("html,body").animate({
+    scrollTop: $(".main-block").offset().top
+  });
+});
