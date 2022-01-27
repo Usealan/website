b6ffbf48f7f84502e6ebeeecdbeeb2d5d255d7c8
Jorge <jorge@mexbowl.com>
2022-01-27 14:31:06 -0600

    conversation with rescheduling

--- hairclub/main.js
+++ hairclub/main.js
@@ -240,37 +240,37 @@ function stepThree() {
 
   setTimeout(function(){
     $(".step[data-step=3] .paths").append("\
-      <div class='date fade-in'><strong>Fri, Jan 28</strong>, 3:00 PM</div>\
+      <div class='date fade-in'><strong>Fri, Jan 28</strong>, 10:48 AM</div>\
     ");
     scrollDown();
   }, 44000);
 
   setTimeout(function(){
     $(".step[data-step=3] .paths").append("\
-      <p class='from-them timeout-09 typing-indicator fade-in'>\
-        <span class='dots'><dot></dot><dot></dot><dot></dot></span>\
-        <span class='text'>Friendly reminder for our appointment in about an hour. For making the time to meet with me I will be providing a complimentary gift card. Talk soon!</span>\
-      </p>\
+      <p class='from-me fade-in'>Hey Jennifer, I'm sorry but I won't be able to make our appointment. My kid is sick and I need to take him to the doctor this afternoon.</p>\
     ");
     scrollDown();
   }, 46000);
 
   setTimeout(function(){
-    $(".timeout-09 .dots").remove();
-    $(".timeout-09 .text").show();
+    $(".step[data-step=3] .paths").append("\
+      <p class='from-them timeout-09 typing-indicator fade-in'>\
+        <span class='dots'><dot></dot><dot></dot><dot></dot></span>\
+        <span class='text'>Hi " + firstName + ", I'm sorry that your kid is sick, I hope he feels better. And please, don't worry about it, we can easily reschedule. Does Monday the 31st at the same work for you?</span>\
+      </p>\
+    ");
     scrollDown();
   }, 48000);
 
   setTimeout(function(){
-    $(".step[data-step=3] .paths").append("\
-      <p class='from-me fade-in'>👍</p>\
-    ");
+    $(".timeout-09 .dots").remove();
+    $(".timeout-09 .text").show();
     scrollDown();
   }, 50000);
 
   setTimeout(function(){
     $(".step[data-step=3] .paths").append("\
-      <div class='date fade-in'><strong>Fri, Jan 28</strong>, 3:30 PM</div>\
+      <p class='from-me fade-in'>Thanks! Yes, that works fine for me.</p>\
     ");
     scrollDown();
   }, 52000);
@@ -279,7 +279,7 @@ function stepThree() {
     $(".step[data-step=3] .paths").append("\
       <p class='from-them timeout-10 typing-indicator fade-in'>\
         <span class='dots'><dot></dot><dot></dot><dot></dot></span>\
-        <span class='text'>Also - text me &quot;here&quot; when you are walking inside so I can make sure to not keep you waiting. Can't wait to chat in about 30 mins!</span>\
+        <span class='text'>Got that rescheduled! Looking forward to meeting with you on the 31st at 4pm.</span>\
       </p>\
     ");
     scrollDown();
@@ -290,13 +290,66 @@ function stepThree() {
     $(".timeout-10 .text").show();
     scrollDown();
   }, 56000);
+  
+  setTimeout(function(){
+    $(".step[data-step=3] .paths").append("\
+      <div class='date fade-in'><strong>Mon, Jan 31</strong>, 3:00 PM</div>\
+    ");
+    scrollDown();
+  }, 58000);
+
+  setTimeout(function(){
+    $(".step[data-step=3] .paths").append("\
+      <p class='from-them timeout-11 typing-indicator fade-in'>\
+        <span class='dots'><dot></dot><dot></dot><dot></dot></span>\
+        <span class='text'>Good afternoon " + firstName +"! I hope your son is feeling better. Just wanted to give you a friendly reminder for our appointment in about an hour. For making the time to meet with me I will be providing a complimentary gift card. Talk soon!</span>\
+      </p>\
+    ");
+    scrollDown();
+  }, 60000);
+
+  setTimeout(function(){
+    $(".timeout-11 .dots").remove();
+    $(".timeout-11 .text").show();
+    scrollDown();
+  }, 62000);
+
+  setTimeout(function(){
+    $(".step[data-step=3] .paths").append("\
+      <p class='from-me fade-in'>👍</p>\
+    ");
+    scrollDown();
+  }, 64000);
+
+  setTimeout(function(){
+    $(".step[data-step=3] .paths").append("\
+      <div class='date fade-in'><strong>Mon, Jan 31</strong>, 3:30 PM</div>\
+    ");
+    scrollDown();
+  }, 66000);
+
+  setTimeout(function(){
+    $(".step[data-step=3] .paths").append("\
+      <p class='from-them timeout-10 typing-indicator fade-in'>\
+        <span class='dots'><dot></dot><dot></dot><dot></dot></span>\
+        <span class='text'>Also - text me &quot;here&quot; when you are walking inside so I can make sure to not keep you waiting. Can't wait to chat in about 30 mins!</span>\
+      </p>\
+    ");
+    scrollDown();
+  }, 68000);
+
+  setTimeout(function(){
+    $(".timeout-10 .dots").remove();
+    $(".timeout-10 .text").show();
+    scrollDown();
+  }, 70000);
 
   setTimeout(function(){
     $(".step[data-step=3] .paths").append("\
       <p class='from-me fade-in'>Here</p>\
     ");
     scrollDown();
-  }, 58000);
+  }, 72000);
 
 }
 
