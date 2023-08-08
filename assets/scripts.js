39beec13e7562094bac721ec03947b2cc3aa9135
Jorge <jorge@mexbowl.com>
2023-08-07 21:30:45 -0500

    new signup form

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,13 +3,24 @@ $(".coming-soon-alert").click(function(e) {
   alert($(this).attr("data-alert"));
 });
 
-$(".go-to-main-block").on("click", function() {
-  event.preventDefault();
+$(".go-to-main-block").on("click", function(e) {
+  e.preventDefault();
   $("body").removeClass("nav-active");
   $("html,body").removeClass("frozen");
   $("html,body").animate({
     scrollTop: $(".main-block").offset().top
   });
+});
+
+$(".open-signup-modal").on("click", function(e) {
+  e.preventDefault();
+  $("body").addClass("signup-on");
+  
+});
+
+$(".close-signup-modal").on("click", function(e) {
+  e.preventDefault();
+  $("body").removeClass("signup-on");
 });
 
 $(".fade-in-alt").fadeIn();
@@ -23,3 +34,25 @@ setTimeout(function(){
   $(".from-them-two .dots").remove();
   $(".from-them-two .text").show();
 }, 9500);
+
+// Sign up form
+
+$("#form").on("submit", function(e) {
+  e.preventDefault();
+  var data = new FormData(this);
+  var action = $(this).attr("action");
+  $.ajax({
+    url: action,
+    method: "POST",
+    data: data,
+    processData: false,
+    contentType: false,
+    success: function() {
+      $(".signup-modal form").hide();
+      $(".signup-modal p").show();
+    },
+    error: function() {
+      alert("An error occurred while submitting the form.");
+    }
+  });
+});
