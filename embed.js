72f96bc42c09925566c228cd0f305a7c7640fc84
Jorge <jorge@mexbowl.com>
2024-09-13 08:23:25 -0500

    ics viewer test

---
+++ embed.js
@@ -0,0 +1,11 @@
+/*
+  
+==================
+EXAMPLE EMBED CODE
+==================
+
+<div data-engine="usealan" data-user-id-short="XXXXXX" data-user-id-long="XXXXXX">
+  <script src="https://usealan.com/embed.js"></script>
+</div>
+  
+*/
